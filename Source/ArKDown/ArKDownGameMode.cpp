dea3c0c0f7a4af9cb2bb9d763a3f20a292fba714
BLaZeKiLL <laldevashish@gmail.com>
2018-07-01 13:58:20 +0530

    FPS character removed Map moved out of LFS

--- Source/ArKDown/ArKDownGameMode.cpp
+++ Source/ArKDown/ArKDownGameMode.cpp
@@ -9,7 +9,7 @@ AArKDownGameMode::AArKDownGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
